650797734df7fc25b39630d060d96974ef465822
Paul Frank Pacheco Carpio <paul.frankpc@gmail.com>
2018-09-26 09:44:34 -0500

    Create scriptBD.sql

---
+++ scriptBD.sql
@@ -0,0 +1,42 @@
+create database dw_example2
+-----------------------------
+use dw_example2
+-----------------------------
+create table articles(
+id int primary key,
+name nvarchar(50),
+provider int,
+category int,
+price money,
+categoryName nvarchar(30)
+)
+----
+delete from articles;
+----------------
+create table articles2(
+id int primary key,
+name nvarchar(50),
+provider int,
+category int,
+price money,
+categoryName nvarchar(30)
+)
+---------------------
+create table articles3(
+id int primary key,
+name nvarchar(50),
+provider int,
+category int,
+price money,
+categoryName nvarchar(30)
+)
+-------------------------
+create table articles4(
+id int primary key,
+name nvarchar(50),
+provider int,
+category int,
+price money,
+categoryName nvarchar(30)
+)
+---select * from articles;
